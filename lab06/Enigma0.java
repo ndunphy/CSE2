0ec4e449c553ade5638b7eb36c4db59ed7c573c3
ndunphy <nrd218@lehigh.edu>
2014-10-16 22:16:43 +0000

    /updated lab08

--- lab06/Enigma0.java
+++ lab06/Enigma0.java
@@ -14,20 +14,29 @@
 import java.util.Scanner;
 public class Enigma0{
   public static void main(String arg []){
-    System.out.print("Enter an int- ");
-    Scanner scan=new Scanner(System.in);
+    
+    
+    Scanner scan;
+    scan=new Scanner(System.in);
    
+    
+    System.out.print("Enter an int- ");
+     int n;
+    n=scan.nextInt(); //moved to after scanner and after prompt
+     //has to be after scanner
+    //took out of if statement
+    
     if(scan.hasNextInt()){
-      int n=scan.nextInt();
+      
       System.out.println("You entered "+n);
     }
     else{
-      int n=4;
+      n=4; //took out "int"
       System.out.println("You entered "+n);
     }
     
-    int k=4,p=6,q=7,r=8,n;
-    n=scan.nextInt();
+    int k=4,p=6,q=7,r=8; //took out n
+    
     switch(k+p+q+r){
       case 24: 
       case 25: System.out.println("sum is 25");
