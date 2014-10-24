661f50c78cd28dbe90504002d3b3f6c51133aab1
ndunphy <nrd218@lehigh.edu>
2014-10-24 15:31:57 +0000

    /updated lab09

--- lab09/Methods.java
+++ lab09/Methods.java
@@ -4,42 +4,42 @@
 //Methods
 //Purpose of program: To write three methods to compare two inputted integers and then compare those to another inputted integer, and evaluate whether they are in ascending order
 
-import java.util.Scanner;
-public class Methods{
-    public static void main(String[] arg){
-        System.out.println("Enter three ints");
-        Scanner scan=new Scanner(System.in);
-        int a, b, c;
+import java.util.Scanner; //import scanner
+public class Methods{ //class methods
+    public static void main(String[] arg){ //main method
+        System.out.println("Enter three ints"); //prompt user
+        Scanner scan=new Scanner(System.in); //declare/initialize scanner
+        int a, b, c; //declare a,b,c
        
-        a=getInt(scan);
+        a=getInt(scan); //initialize a,b,c as the integers from the method getInt()
         
         b=getInt(scan);
         
         c=getInt(scan);
         
-        System.out.println("The larger of "+a+" and "+b+" is "+larger(a,b));
-        System.out.println("The larger of "+a+", "+b+", and "+c+ " is "+larger(a,larger(b,c)));
-        System.out.println("It is "+ascending(a,b,c)+" that "+a+", "+b+ ", and "+c+" are in ascending order");
+        System.out.println("The larger of "+a+" and "+b+" is "+larger(a,b)); //print largest of first two integers
+        System.out.println("The larger of "+a+", "+b+", and "+c+ " is "+larger(a,larger(b,c))); //largest of all integers
+        System.out.println("It is "+ascending(a,b,c)+" that "+a+", "+b+ ", and "+c+" are in ascending order"); //evaluating validity of ascending order in order from smallest to biggest
     
              
      }
        
-    public static int getInt(Scanner scan){
+    public static int getInt(Scanner scan){ //getInt method
       
-        System.out.println("Enter an int- ");
+        System.out.println("Enter an int- "); //prompt user
      
-       Scanner scan1= new Scanner( System.in);
-       int myScanner= 0;
-       if(scan.hasNextInt()){
+       Scanner scan1= new Scanner( System.in); //scanner object
+       int myScanner= 0; //declare and initialize myScanner
+       if(scan.hasNextInt()){ //test input as an integer
        
       
          myScanner= scan.nextInt();
       
     
-        return myScanner;
+        return myScanner; //return value inputted into scanner
        
     }
-    else{
+    else{ //if not an integer print error and have user enter another int
         System.out.println("You did not enter an int! Try again- ");
         String junk= scan.next();
         return getInt(scan);
@@ -48,35 +48,35 @@ public class Methods{
     }
     
     
-    public static int larger(int n1, int n2){
-        int max=0;
-        if (n1>=n2){
-            max= n1;
+    public static int larger(int n1, int n2){ //larger method
+        int max=0; //initialize and declare max
+        if (n1>=n2){ //if the first number is larger that is the max
+            max= n1; //n1 is set as max
         
             
         }
-        else if (n2>=n1) {
-            max= n2;
+        else if (n2>=n1) { //if second number is larger that is the max
+            max= n2; //n2 is set as max
     
         }
-        return max;
+        return max; //return the value of the max
     }
     
-    public static boolean ascending(int a, int b, int c){
-        int n3=larger(a, larger(b,c));
-        int n2=larger(a,b);
-        int n1=a;
+    public static boolean ascending(int a, int b, int c){ //ascending method
+        int n3=larger(a, larger(b,c)); //set n3 to largest number of three
+        int n2=larger(a,b); //set n2 to second largest number
+        int n1=a; //set n1 to remaining number
         
-       boolean order=true;
+       boolean order=true; //initialize and declare order
        
-        if (n1==a&& n2==b && n3==c){
-            order= true;
+        if (n1==a&& n2==b && n3==c){ //if the value of the largest integer is a, second largest is b, and last is c then the boolean is true
+            order= true; //sets order to true
         }
-        else {
-           order=false;
+        else { //if its not in that order
+           order=false; //order is set to false
         }
         
-        return order;
+        return order; //the value of order (true or false) is returned
        
     }
     
